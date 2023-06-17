4c96ab01889d38264cbf2bc5de3d94734a6739ca
anlele1701 <lethanhduyan@gmail.com>
2023-06-17 12:18:04 +0700

    dzien

--- 3dobject/3dobject/App_Start/RouteConfig.cs
+++ 3dobject/3dobject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _3dobject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MyFiles", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
